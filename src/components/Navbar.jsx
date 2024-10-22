8faa5ac5d01d5a55f49d4f3ba07fbaabf4bbad85
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-10-22 23:47:47 +0000

    bug fixing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { useNavigate, useLocation } from 'react-router-dom'
 //import { ExploreIcon, OfferIcon, PersonOutlineIcon } from './Icons'
-import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
-import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
-import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
+import OfferIcon from '../assets/svg/localOfferIcon.svg?react'
+import ExploreIcon from '../assets/svg/exploreIcon.svg?react'
+import PersonOutlineIcon from '../assets/svg/personOutlineIcon.svg?react'
 
 const Navbar = () => {
   return (
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// ADD THIS TO ENABLE SVGS
+import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  // …
   plugins: [react(), svgr()],
 })
