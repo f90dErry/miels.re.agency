60cc5f3163ffc3e742e313e0182fa47319eb4f99
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-10-22 01:15:54 +0000

    fixing vite issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,14 @@
 import { useNavigate, useLocation } from 'react-router-dom'
-import { ExploreIcon, OfferIcon, PersonOutlineIcon } from './Icons'
-// import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
-// import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
-// import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
+//import { ExploreIcon, OfferIcon, PersonOutlineIcon } from './Icons'
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
+import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
 
 const Navbar = () => {
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
-        <ul
-          className='navListItems'
-          style={{
-            display: 'flex',
-            gap: '20px',
-          }}
-        >
+        <ul className='navListItems'>
           <li className='navbarListItem'>
             <ExploreIcon fill='2c2c2c' width='36px' height='36px' />
             <p>Explore</p>
