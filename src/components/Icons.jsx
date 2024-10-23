ed580022996c6db6def73a01a08fd9d70e0c1919
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-10-23 00:32:09 +0000

    bug fixed
    gave a className of navListItem instead of navbarListItems

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export const OfferIcon = () => {
+export const OfferIcon = ({ fill, height, width }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      height='36px'
+      height={height}
       viewBox='0 0 24 24'
-      width='36px'
-      fill='#2c2c2c'
+      width={width}
+      fill={fill}
     >
       <path d='M0 0h24v24H0V0z' fill='none' />
       <path d='M21.41 11.58l-9-9C12.05 2.22 11.55 2 11 2H4c-1.1 0-2 .9-2 2v7c0 .55.22 1.05.59 1.42l9 9c.36.36.86.58 1.41.58s1.05-.22 1.41-.59l7-7c.37-.36.59-.86.59-1.41s-.23-1.06-.59-1.42zM13 20.01L4 11V4h7v-.01l9 9-7 7.02z' />
@@ -16,14 +16,14 @@ export const OfferIcon = () => {
   )
 }
 
-export const ExploreIcon = () => {
+export const ExploreIcon = ({ fill, height, width }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      height='36px'
+      height={height}
       viewBox='0 0 24 24'
-      width='36px'
-      fill='#2c2c2c'
+      width={width}
+      fill={fill}
     >
       <path d='M0 0h24v24H0V0z' fill='none' />
       <path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5.5-2.5l7.51-3.49L17.5 6.5 9.99 9.99 6.5 17.5zm5.5-6.6c.61 0 1.1.49 1.1 1.1s-.49 1.1-1.1 1.1-1.1-.49-1.1-1.1.49-1.1 1.1-1.1z' />
@@ -31,14 +31,14 @@ export const ExploreIcon = () => {
   )
 }
 
-export const PersonOutlineIcon = () => {
+export const PersonOutlineIcon = ({ fill, height, width }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      height='36px'
+      height={height}
       viewBox='0 0 24 24'
-      width='36px'
-      fill='#2c2c2c'
+      width={width}
+      fill={fill}
     >
       <path d='M0 0h24v24H0V0z' fill='none' />
       <path d='M12 6c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2m0 10c2.7 0 5.8 1.29 6 2H6c.23-.72 3.31-2 6-2m0-12C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 10c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z' />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
 import { useNavigate, useLocation } from 'react-router-dom'
-//import { ExploreIcon, OfferIcon, PersonOutlineIcon } from './Icons'
-import OfferIcon from '../assets/svg/localOfferIcon.svg?react'
-import ExploreIcon from '../assets/svg/exploreIcon.svg?react'
-import PersonOutlineIcon from '../assets/svg/personOutlineIcon.svg?react'
+import { ExploreIcon, OfferIcon, PersonOutlineIcon } from './Icons'
+//import OfferIcon from '../assets/svg/localOfferIcon.svg?react'
+//import ExploreIcon from '../assets/svg/exploreIcon.svg?react'
+//import PersonOutlineIcon from '../assets/svg/personOutlineIcon.svg?react'
 
 const Navbar = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
-        <ul className='navListItems'>
-          <li className='navbarListItem'>
+        <ul className='navbarListItems'>
+          <li className='navbarListItem' onClick={() => navigate('/')}>
             <ExploreIcon fill='2c2c2c' width='36px' height='36px' />
             <p>Explore</p>
           </li>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/offers')}>
             <OfferIcon fill='2c2c2c' width='36px' height='36px' />
             <p>Offer</p>
           </li>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/profile')}>
             <PersonOutlineIcon fill='2c2c2c' width='36px' height='36px' />
             <p>Profile</p>
           </li>
