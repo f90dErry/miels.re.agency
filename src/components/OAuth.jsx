dc13ed0540bcacba7f51f34bcb0105b0bc764347
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-10-31 13:13:26 +0000

    sign up with google

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from 'react-router-dom'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
-import { doc, setDoc, getDoc } from 'firebase/firestore'
+import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
 import { GoogleIcon } from './Icons'
@@ -9,13 +9,36 @@ const OAuth = () => {
   const navigate = useNavigate()
   const location = useLocation()
 
-  const onGoogleClick = (e) => {}
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+
+      //check for user
+      const docRef = doc(db, 'users', user.uid)
+      const docSnap = await getDoc(docRef)
+
+      //if user doesn't exist, create user
+      if (!docSnap.exists()) {
+        await setDoc(doc(db), 'users', user.uid, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        })
+      }
+      navigate('/')
+    } catch (error) {
+      toast.error('something went wrong')
+    }
+  }
 
   return (
     <div className='socialLogin'>
       <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with</p>
       <button className='socialIconDiv' onClick={onGoogleClick}>
-        <img className='socialIconImg' src={GoogleIcon} alt='google' />
+        <GoogleIcon className='socialIconImg' />
       </button>
     </div>
   )
