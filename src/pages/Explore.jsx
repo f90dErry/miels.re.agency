ded8fb094cba28647f952444280c726ce1081536
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-11-02 10:53:56 +0000

    explore page
    start building the explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 const Explore = () => {
   return (
-    <div>
-      <h1>Explore</h1>
+    <div className='explore'>
+      <header className=''>
+        <p className='pageHeader'>Explore</p>
+      </header>
+
+      <main>
+        {/* slider */}
+
+        <p className='exploreCategoryHeading'>Categories</p>
+        <div className='p exploreCategories'>
+          <Link to='/category/rent'>
+            <img
+              src={rentCategoryImage}
+              alt='rent'
+              className='exploreCategoryImg'
+            />
+          </Link>
+          <Link to='/category/sell'>
+            <img
+              src={sellCategoryImage}
+              alt='sell'
+              className='exploreCategoryImg'
+            />
+          </Link>
+        </div>
+      </main>
     </div>
   )
 }
