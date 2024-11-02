8982e5dfc5b1da5cba49488e70456ebe185ab898
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-11-02 12:31:50 +0000

    show listing on category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -66,7 +66,15 @@ const Category = () => {
       {loading ? (
         <Spinner />
       ) : listings && listings.length > 0 ? (
-        <></>
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <h3 key={listing.id}>{listing.data.name}</h3>
+              ))}
+            </ul>
+          </main>
+        </>
       ) : (
         <p>No listings for {params.categoryName}</p>
       )}
