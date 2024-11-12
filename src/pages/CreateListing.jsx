fb0f14d8a5c93ef33effdbcafd65c62b6f72cb95
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-11-12 12:11:18 +0000

    create listing page
    building up create listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -22,6 +22,22 @@ const CreateListing = () => {
     longitude: 0,
   })
 
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    offer,
+    regularPrice,
+    discountPrice,
+    images,
+    latitude,
+    longitude,
+  } = formData
+
   const auth = getAuth()
   const navigate = useNavigate()
   const isMounted = useRef()
@@ -42,10 +58,150 @@ const CreateListing = () => {
     // eslint-disabled-next-line react-hooks/exhautive-deps
   }, [isMounted])
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const onMutate = (e) => {}
+
   if (loading) {
     return <Spinner />
   }
-  return <div>Create</div>
+  return (
+    <div className='profile'>
+      <header>
+        <p className='pageHeader'>Create a listing</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <label className='formLabel'>Sell / Rent</label>
+          <div className='formButtons'>
+            <button
+              type='button'
+              className={type === 'sale' ? 'formButtonActive' : 'formButton'}
+              id='type'
+              value='sale'
+              onClick={onMutate}
+            >
+              Sell
+            </button>
+            <button
+              type='button'
+              className={type === 'rent' ? 'formButtonActive' : 'formButton'}
+              id='type'
+              value='rent'
+              onClick={onMutate}
+            >
+              Rent
+            </button>
+          </div>
+
+          <label className='formLabel'>Name</label>
+          <input
+            type='text'
+            className='formInputName'
+            id='name'
+            value={name}
+            onChange={onMutate}
+            maxLength='32'
+            minLength='10'
+            required
+          />
+        </form>
+        <div className='formRooms flex'>
+          <div>
+            <label className='formLabel'>Bedrooms</label>
+            <input
+              type='number'
+              className='formInputSmall'
+              id='bedrooms'
+              value={bedrooms}
+              onChange={onMutate}
+              min='1'
+              max='50'
+              required
+            />
+          </div>
+          <div>
+            <label className='formLabel'>Bathrooms</label>
+            <input
+              type='number'
+              className='formInputSmall'
+              id='bathrooms'
+              value={bathrooms}
+              onChange={onMutate}
+              min='1'
+              max='50'
+              required
+            />
+          </div>
+        </div>
+
+        <label className='formLabel'>Parking spot</label>
+        <div className='formButtons'>
+          <button
+            className={parking ? 'formButtonActive' : 'formButton'}
+            type='button'
+            id='parking'
+            value={true}
+            onClick={onMutate}
+            min='1'
+            max='5'
+          >
+            Yes
+          </button>
+          <button
+            className={
+              !parking && parking !== null ? 'formButtonActive' : 'formButton'
+            }
+            type='button'
+            value={false}
+            id='parking'
+            onClick={onMutate}
+          >
+            NO
+          </button>
+        </div>
+
+        <label className='formLabel'>Furnished</label>
+        <div className='formButtons'>
+          <button
+            className={furnished ? 'formButtonActive' : 'formButton'}
+            type='button'
+            id='furnished'
+            value={true}
+            onClick={onMutate}
+          >
+            Yes
+          </button>
+          <button
+            className={
+              !furnished && furnished !== null
+                ? 'formButtonActive'
+                : 'formButton'
+            }
+            type='button'
+            value={false}
+            id='furnished'
+            onClick={onMutate}
+          >
+            NO
+          </button>
+        </div>
+
+        <label className='formLabel'>Address</label>
+        <textarea
+          className='formInputAddress'
+          id='address'
+          value={address}
+          type='text'
+          onClick={onMutate}
+          required
+        ></textarea>
+      </main>
+    </div>
+  )
 }
 
 export default CreateListing
